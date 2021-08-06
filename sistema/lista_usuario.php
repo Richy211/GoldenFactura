8600b40ee25e6c3a0d9ce79ae0502beb177570cd
Richy211 <ricardollanos1@gmail.com>
2021-08-06 17:42:46 -0400

    Se crea el Query del paginador. Se configura los botones

--- sistema/lista_usuario.php
+++ sistema/lista_usuario.php
@@ -26,8 +26,27 @@
 
 <?php 
 
+	//Paginador
+	$sql_registe = mysqli_query($conection,"SELECT COUNT(*) as total_registro FROM usuario WHERE estatus = 1 ");
+	$result_register = mysqli_fetch_array($sql_registe);
+	$total_registro = $result_register['total_registro'];
+
+	$por_pagina = 5;
+
+	if(empty($_GET['pagina']))
+	{
+		$pagina = 1;
+	}else{
+		$pagina = $_GET['pagina'];
+	}
+
+		$desde = ($pagina-1) * $por_pagina;
+		$total_paginas = ceil($total_registro / $por_pagina);
+
 	$query = mysqli_query($conection, "SELECT u.idusuario, u.nombre, u.correo, u.usuario, r.rol 
-						FROM usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE estatus = 1  ORDER BY idusuario ASC");
+						FROM usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE estatus = 1
+						ORDER BY u.idusuario 
+						ASC LIMIT $desde,$por_pagina ");
 
 	$result = mysqli_num_rows($query);
 	if($result > 0){
@@ -65,15 +84,30 @@
 
 		<div class="paginador">
 			<ul>
-				<li><a href=""> |< </a> </li>
-				<li> <a href=""> << </a></li>
-				<li class="pageSelected"> 1 </a></li>
-				<li><a href=""> 2 </a></li>
-				<li><a href=""> 3 </a></li>
-				<li><a href=""> 4 </a></li>
-				<li><a href=""> 5 </a></li>
-				<li><a href=""> >> </a></li>
-				<li><a href=""> >| </a></li>
+			<?php 
+				if($pagina != 1)
+				{
+			?>
+				<li><a href="?pagina=<?php echo 1; ?>"> |< </a> </li>
+				<li><a href="?pagina=<?php echo $pagina-1; ?>"> << </a></li>
+			<?php
+				}
+						for ($i=1; $i <= $total_paginas; $i++){
+							if($i == $pagina)
+							{
+								echo '<li class="pageSelected">'.$i.'</li>';
+							}else{
+								echo '<li><a href="?pagina='.$i.'">'.$i.'</a></li>';
+							}							
+						}
+
+					if($pagina != $total_paginas)
+					{
+					?>
+
+				<li><a href="?pagina=<?php echo $pagina + 1; ?>"> >> </a></li>
+				<li><a href="?pagina=<?php echo $total_paginas; ?>"> >| </a></li>
+				<?php } ?>
 			</ul>
 		</div>
 		
