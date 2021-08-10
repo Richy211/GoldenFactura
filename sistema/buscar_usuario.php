a65403292f7e46e3f6a50b4a97252139518624f5
Richy211 <ricardollanos1@gmail.com>
2021-08-10 17:48:58 -0400

    Modificaciones en la pagina de busqueda

--- sistema/buscar_usuario.php
+++ sistema/buscar_usuario.php
@@ -1,38 +1,45 @@
+<?php
 
-<?php include "../conexion.php"; ?>
+	include "../conexion.php";
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-<?php include "includes/scripts.php";?>
-	<title>Lista de usuarios</title>
+	
+	<?php include "includes/scripts.php"; ?>
+
+	<title>Sistema de Ventas</title>
 </head>
 <body>
-	<?php include "includes/header.php";?>
+			<?php include "includes/header.php";?>
 	<section id="container">
 
-        <?php
-        
-        $busqueda = strtolower($_REQUEST['busqueda']);
-        if(empty($busqueda))
-        {
-            header("location: lista_usuario.php");
-        }
-        
-        ?>
+		<?php 
+
+			$busqueda = strtolower($_REQUEST['busqueda']);
+			if(empty($busqueda))
+			{
+				header("location: lista_usuarios.php");
+				mysqli_close($conection);
+			}
+		?>
 
 		<h1>Lista de Usuarios</h1>
-		<a href="registro_usuario.php" class="btn_new">Buscar usuario</a>
+		<a href="registro_usuario.php" class="btn_new">Crear usuario</a>
 
 		<form action="buscar_usuario.php" method="get" class="form_search">
-			<input type="text" name="busqueda" id="busqueda" placeholder="Buscar" value="<?php echo $busqueda; ?>">
-			<input type="submit" value="Buscar" class="btn_search">
+			<input type="text" name="busqueda" id="busqueda" 
+			placeholder="Buscar" value="<?php echo $busqueda; ?>">
+			<input type="submit" value="Buscar" 
+			class="btn_search">
 		</form>
 
 		<table>
 			<tr>
-				<th>Id</th>
+				<th>ID</th>
 				<th>Nombre</th>
 				<th>Correo</th>
 				<th>Usuario</th>
@@ -40,118 +47,130 @@
 				<th>Acciones</th>
 			</tr>
 
-<?php 
-//Paginador
-    $rol = '';
-    if($busqueda == 'administrador')
-    {
-        $rol = " OR rol LIKE '%1%' ";
+			<?php
+			//paginador
+			$rol = '';
 
-    }else if($busqueda == 'supervisor'){
-        
-        $rol = " OR rol LIKE '%2%' ";
+			if($busqueda == 'administrador')
+			{
+				$rol= "OR rol LIKE '%1%' ";
 
-    }else if($busqueda == 'vendedor'){
-        
-        $rol = " OR rol LIKE '%3%' ";
-    }
+			}else if($busqueda == 'supervisor'){
 
-	$sql_registe = mysqli_query($conection,"SELECT COUNT(*) as total_registro FROM usuario 
-                                        WHERE ( idusuario LIKE '%$busqueda%' OR
-                                                    nombre LIKE '%$busqueda%' OR
-                                                    correo LIKE '%$busqueda%' OR 
-                                                    usuario LIKE '%$busqueda%'
-                                                    $rol )
-                                        AND estatus = 1");
+				$rol = "OR rol LIKE '%2%' ";
 
-	$result_register = mysqli_fetch_array($sql_registe);
-	$total_registro = $result_register['total_registro'];
+			}else if($busqueda == 'vendedor'){
 
-	$por_pagina = 5;
+				$rol = " OR rol LIKE '%3%' ";
+			}
 
-	if(empty($_GET['pagina']))
-	{
-		$pagina = 1;
-	}else{
-		$pagina = $_GET['pagina'];
-	}
+			$sql_registe = mysqli_query($conection, "SELECT COUNT(*) as total_registro FROM usuario
+										 WHERE (idusuario LIKE '%$busqueda%' OR
+										 		 nombre LIKE '%$busqueda%' OR
+										 		 correo LIKE '%$busqueda%' OR
+										 		 usuario LIKE '%$busqueda%' 
+										 		 $rol )
+										 AND estatus = 1 " );
 
-		$desde = ($pagina-1) * $por_pagina;
-		$total_paginas = ceil($total_registro / $por_pagina);
+			$result_register = mysqli_fetch_array($sql_registe);
+			$total_registro = $result_register['total_registro'];
 
-	$query = mysqli_query($conection, "SELECT u.idusuario, u.nombre, u.correo, u.usuario, r.rol 
-						FROM usuario u INNER JOIN rol r ON u.rol = r.idrol 
-                        WHERE 
-                        ( u.idusuario LIKE '%$busqueda%' OR
-                        u.nombre  LIKE   '%$busqueda%' OR
-                        u.correo LIKE    '%$busqueda%' OR 
-                        u.usuario LIKE   '%$busqueda%' OR
-                        u.rol     LIKE   '%$busqueda%' )
-                        AND estatus = 1 ORDER BY u.idusuario ASC LIMIT $desde,$por_pagina ");
+			$por_pagina = 5;
 
-	$result = mysqli_num_rows($query);
-	if($result > 0){
+			if(empty($_GET['pagina'])) //SI el valor pasado por el metodo GEt no existe la pagina va a ser 1
+			{
+				$pagina = 1;
+			}else{
+				$pagina = $_GET['pagina'];
+			}
 
-		while($data = mysqli_fetch_array($query)){
-?>
+			$desde = ($pagina - 1) * $por_pagina;
+			$total_paginas = ceil($total_registro / $por_pagina);
+
+			 $query = mysqli_query($conection, "SELECT u.idusuario, u.nombre, u.correo, u.usuario, r.rol 
+			 	FROM usuario u INNER JOIN rol r ON u.rol = r.idrol 
+			 	WHERE 
+			 			(u.idusuario LIKE '%$busqueda%' OR
+						u.nombre LIKE '%$busqueda%' OR
+						u.correo LIKE '%$busqueda%' OR
+						u.usuario LIKE '%$busqueda%' OR
+						r.rol LIKE '%$busqueda%' )
+						AND 
+			 	estatus = 1 ORDER BY u.idusuario ASC LIMIT $desde,$por_pagina");
+
+			 
+			 mysqli_close($conection);
+			 $result = mysqli_num_rows($query);
+			 if($result > 0){
+
+			 	while($data = mysqli_fetch_array($query))  {
+
+			?>
 
 			<tr>
-				<td><?php echo $data['idusuario']; ?></td>
-				<td><?php echo $data['nombre']; ?></td>
-				<td><?php echo $data['correo']; ?></td>
-				<td><?php echo $data['usuario']; ?></td>
-				<td><?php echo $data['rol']; ?></td>
-				<td>
-					<a class="link_edit" href="editar_usuario.php?id=<?php echo $data['idusuario'];?>">Editar</a>
-					||
+			<td><?php echo $data['idusuario'];?></td>
+			<td><?php echo $data['nombre'];?></td>
+			<td><?php echo $data['correo'];?></td>
+			<td><?php echo $data['usuario'];?></td>
+			<td><?php echo $data['rol'];?></td>
+			<td>
+				<a class="link_edit" href="editar_usuarios.php?id=<?php echo $data['idusuario'];?>">Editar</a>
 
-					<!-- Se crea esta proteccion para que los que no son el admin o id = 1 no se muester el boton de borrar -->
-					<?php if($data["idusuario"] != 1) {?>
+				<!-- lo que hace es ocultar el boton de eliminar en el super administrador quien tiene el el id que es igual a 1 -->
+				<?php if($data['idusuario'] != 1 ){?>
+				<a class="link_delete" href="eliminar_confirmar_usuario.php?id=<?php echo $data['idusuario'];?>">Eliminar</a>
 
-					<a class="link_delete" href="eliminar_confirmar_usuario.php?id=<?php echo $data['idusuario'];?>">Eliminar</a>
+			<?php } ?>
+			</td>
 
-					<?php } ?>
-
-				</td>
-			</tr>
 
 			<?php
-		}
-	}
-
- ?>
+			 	}
+			 }
+			?>
 
 		</table>
 
+<?php 
+	if($total_registro != 0)
+	{
+ ?>
+
+
 		<div class="paginador">
 			<ul>
-			<?php 
-				if($pagina != 1)
-				{
-			?>
-				<li><a href="?pagina=<?php echo 1; ?>"> |< </a> </li>
-				<li><a href="?pagina=<?php echo $pagina-1; ?>"> << </a></li>
-			<?php
-				}
-						for ($i=1; $i <= $total_paginas; $i++){
-							if($i == $pagina)
-							{
-								echo '<li class="pageSelected">'.$i.'</li>';
-							}else{
-								echo '<li><a href="?pagina='.$i.'">'.$i.'</a></li>';
-							}							
-						}
-
-					if($pagina != $total_paginas)
+				<?php 
+					if($pagina != 1)//Si pagina no es igual a uno, se muestran estos primeros botones, de lo contrario desaparecen
 					{
 					?>
+				<li><a href="?pagina=<?php echo 1; ?>&busqueda=<?php echo $busqueda; ?>">|<</a></li>
+				<li><a href="?pagina=<?php echo $pagina-1; ?>&busqueda=<?php echo $busqueda; ?>"><<</a></li>
+			 <?php 
+			 		}
+			 	for($i=1; $i <= $total_paginas; $i++){
 
-				<li><a href="?pagina=<?php echo $pagina + 1; ?>"> >> </a></li>
-				<li><a href="?pagina=<?php echo $total_paginas; ?>"> >| </a></li>
-				<?php } ?>
+			 		if($i == $pagina)
+			 		{
+			 			 echo '<li class="pageSelected">'.$i.'</a></li>';
+			 		}else{
+			 			echo '<li><a href="?pagina='.$i.'&busqueda='.$busqueda.'">'.$i.'</a></li>';
+			 		}
+			 		
+			 	}
+
+			 	if($pagina != $total_paginas)
+			 	{
+			  ?>
+				<li><a href="?pagina=<?php echo $pagina + 1;?> &busqueda=<?php echo $busqueda; ?>">>></a></li>
+				<li><a href="?pagina=<?php echo $total_paginas;?>&busqueda=<?php echo $busqueda; ?>">>|</a></li>
+			<?php } ?>
 			</ul>
 		</div>
-		
+<?php } ?>
+
 	</section>
+
+
+
 </body>
 </html>
