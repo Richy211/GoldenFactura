54cb5b477a7a4d33972178224c1a7aa8d8c2f0ed
Richy211 <ricardollanos1@gmail.com>
2021-08-20 22:17:02 -0400

    SUbida del logo y  cracion de cabecera en pdf.

--- sistema/index2.php
+++ sistema/index2.php
@@ -10,35 +10,58 @@
 	$pdf->AliasNbPages();
 	$pdf->AddPage();
 
-	$pdf->SetFillColor(43, 107, 229);
+	$pdf->SetFillColor(255,255, 255);
 	$pdf->Rect(0,0,220,40, 'F');
 
 
 	$pdf->SetFont('Arial','b', 20);
-	$pdf->SetY(10);
-	$pdf->SetX(5);
-	$pdf->SetTextColor(255,255,255);
-	$pdf->write(5, 'Cotizacion Formal');
+	$pdf->SetY(20);
+	$pdf->SetX(70);
+	$pdf->SetTextColor(0,0,0);
+	$pdf->write(5, 'GoldenClass');
+	$pdf->Ln();
 
-	$pdf->SetFont('Arial','',10);
-	$pdf->SetY(10);
-	$pdf->Setx(120);
+	$pdf->Ln();
+	$pdf->SetFont('Arial','b',10);
+	$pdf->SetX(60);
+	$pdf->write(5, 'Salvador Reyes 150, Estacion Central');
+	$pdf->Ln();
+	
+	$pdf->SetFont('Arial','b',10);
+	$pdf->SetX(60);
+	$pdf->write(5,'Rut:15432543-6');
+	$pdf->Ln();
+
+	$pdf->SetFont('Arial','b',10);
+	$pdf->SetX(60);
+	$pdf->write(5,'Telefono:987987987');
+	$pdf->Ln();
+
+	$pdf->SetFont('Arial','b',10);
+	$pdf->SetX(60);
+	$pdf->write(5,'Email:nadiavergara@goldenclass.com');
+	$pdf->Ln();
+	
+
+	$pdf->SetFont('Arial','b',10);
+	$pdf->SetY(30);
+	$pdf->Setx(150);
 	$pdf->write(5, 'Detalle del Envio: ');
 	$pdf->Ln();
-	$pdf->Setx(120);
+	$pdf->Setx(150);
 	$pdf->write(5, 'Fecha: ');
 	$pdf->Ln();
-	$pdf->Setx(120);
+	$pdf->Setx(150);
 	$pdf->write(5, 'Hora: ');
 	$pdf->Ln();
-	$pdf->Setx(120);
+	$pdf->Setx(150);
 	$pdf->write(5, 'Vendedor: ');
 	$pdf->Ln();
 	$pdf->Ln(20);
 
 	$pdf->SetTextColor(0,0,0);
-	$pdf->SetY(50);
-	$pdf->Image('img/empresa.png',20);
+	$pdf->SetY(60);
+	$pdf->Image('img/logoGolden_ok.png',20,30,17,17,'png');
 
 
 	
